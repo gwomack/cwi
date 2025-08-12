7744396e43adb30ac17e432ce208116b0573ae23
gwomack <danielveronese@gmail.com>
2025-08-12 16:17:31 -0300

    define rotas

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\ApiTokenController;
+use App\Http\Controllers\Api\V1\UserController;
 
+Route::post('v1/token', [ApiTokenController::class, 'generateToken']);
 
-Route::apiResource('users', App\Http\Controllers\Api\V1\UserController::class);
+Route::middleware('auth:sanctum')->prefix('v1')
+    ->group(function () {
+        Route::apiResource('users', UserController::class);
+    });
